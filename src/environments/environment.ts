95ca3ed68910f186b7ffc1d1f879e1f40c41eacf
Arunim Chaudhary <arunim.chaudhary@stackguardian.io>
2024-06-21 17:29:13 +0530

    init ezg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,18 @@ export const environment = {
   production: false,
   t2y12PruGU9wUtEGzBJfolMIgK: 'CHANGEIT',
   VERSION: require('../../package.json').version,
-  remoteConfig: false, 
+  remoteConfig: false,
   remoteConfigUrl: './design-studio-config.json',
-  apiUrl: 'http://localhost:3000/',
-  widgetBaseUrl: 'http://localhost:4200/',
-  dashboardBaseUrl: 'http://localhost:4200/',
+  apiUrl: 'http://localhost:3001/',
+  widgetBaseUrl: 'http://localhost:8090/widget',
+  dashboardBaseUrl: 'http://localhost:8090/',
+  wsUrl: 'ws://localhost:3001/',
   whatsappTemplatesBaseUrl: 'http://localhost:3000/',
-  wsUrl: 'ws://localhost:3000/',
   uploadEngine: 'native',
   baseImageUrl: 'CHANGEIT',
   logLevel: 'error',
   aiModels: 'CHANGEIT',
-  storage_prefix: "cds_sv6",
+  storage_prefix: 'cds_sv6',
   firebaseConfig: {
     apiKey: 'CHANGEIT',
     authDomain: 'CHANGEIT',
@@ -22,6 +22,6 @@ export const environment = {
     storageBucket: 'CHANGEIT',
     messagingSenderId: 'CHANGEIT',
     appId: 'CHANGEIT',
-    vapidKey: 'CHANGEIT'
-  }
+    vapidKey: 'CHANGEIT',
+  },
 };
